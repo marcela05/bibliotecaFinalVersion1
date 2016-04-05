4a845db4cc85264d6269553ce820be6954814c84
Javier Suarez <luisjavier.suarezmeza@gmail.com>
2016-04-05 18:09:06 -0500

    add events handler

--- myfirtsapp.js
+++ myfirtsapp.js
@@ -3,44 +3,70 @@ if (Meteor.isClient) {
   Session.setDefault('counter', 0);
 
   Template.body.helpers({
-    types: function () {
-      return ['a', 'b', 'c', 'd']
+    gender: function () {
+      return ['M', 'F']
     }
-
   });
 
   Template.body.events({
-    // 'change select': function(evt) {
-    //     event.preventDefault();
-    //     var newValue = $(evt.target).val();
-    //     console.log("value: ", newValue);
-    //     if (newValue === 'List') {
-    //         Session.set('fieldType', newValue);
-    //     } else {
-            
-    //     }        
-    // },
-
-    // 'keyup #comment': function () {
-    //     var text_max = 120;
-    //     var text_length = $('#comment').val().length;
-    //     var text_remaining = 120;
-    //     if (text_remaining > 0) {
-    //         text_remaining = text_max - text_length;
-    //         $('#comment_feedback').html(text_remaining + ' characters remaining...');        
-    //     } 
-    // },
-
-    // 'blur #comment': function(event) {
-    //     console.log("blur comment");
-    //     event.preventDefault();
-    // },
 
     'submit #form-a': function(event, template){
+        event.preventDefault();        
+        
+        var name = event.target.name.value;
+        var comment = event.target.comment.value;
+        var email = event.target.email.value;
+        var gender = Session.get('gender');
+
+        if (gender === undefined) {
+            alert("select your gender");
+        } else {
+            console.log("Name: ", name);
+            console.log("Comment: ", comment);
+            console.log("Gender: ", gender);
+            console.log("Email: ", email);
+
+            event.target.name.value = '';
+            event.target.comment.value = '';
+            event.target.email.value = '';
+        }        
+    },
+
+    'change select': function(evt) {
+        evt.preventDefault();
+        var newValue = $(evt.target).val();
+
+        Session.set('gender', newValue);
+
+        console.log("value: ", newValue);
+        if (newValue === 'F') {
+            //alert('Es mujer');
+        } else {
+            
+        }        
+    },
+
+    'blur #comment': function(event) {
         event.preventDefault();
-        //var _name = event.target.fupInput.value
+        var _comment = $('[name="comment"]').val();
+        console.log("blur comment", _comment);
+    },
+
+    'blur #name': function(event) {
+        event.preventDefault();
         var _name = $('[name="search"]').val();
-        console.log("submit a: ", _name);
+        console.log("blur name", _name);
+    },
+
+    'keyup #comment': function () {
+        var text_max = 120;
+        var text_length = $('#comment').val().length;
+        var text_remaining = 120;
+        if (text_remaining > 0) {
+            text_remaining = text_max - text_length;
+            $('#comment_feedback').html(text_remaining + 
+                ' characters remaining...');        
+        } 
     },
 
     'submit #form-b': function(event, template){
